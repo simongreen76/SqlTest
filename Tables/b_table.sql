7c6021da2cdeecc45f0aabb143a99b70bae237d4
Simon Green <simon.green@savanta.com>
2025-07-30 17:17:18 +0100

    Made the statements 'create or replace'

--- Tables/b_table.sql
+++ Tables/b_table.sql
@@ -1,4 +1,9 @@
-CREATE TABLE automobile (
+CREATE OR REPLACE SEQUENCE automobile_id_seq
+    START = 1
+    INCREMENT = 1
+    COMMENT = 'Sequence for automobile primary keys';
+
+CREATE OR REPLACE TABLE automobile (
     automobile_id NUMBER DEFAULT automobile_id_seq.NEXTVAL,
     make VARCHAR(50) NOT NULL,
     model VARCHAR(100) NOT NULL,
